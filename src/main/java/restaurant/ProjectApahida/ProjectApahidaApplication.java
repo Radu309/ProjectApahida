118d4bdfc3481d4a2c53ce05d4a38769a5683124
Radu Neacă <neaca_radu309@yahoo.com>
2023-05-10 22:22:40 +0300

    First!

---
+++ src/main/java/restaurant/ProjectApahida/ProjectApahidaApplication.java
@@ -0,0 +1,13 @@
+package restaurant.ProjectApahida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApahidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApahidaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/restaurant/ProjectApahida/ProjectApahidaApplicationTests.java
@@ -0,0 +1,13 @@
+package restaurant.ProjectApahida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApahidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
